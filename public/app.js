32edbf91e211ef420f613419e143acd4b1e785d9
Oli <oli@olillin.com>
2025-02-25 21:40:37 +0100

    Add page content

---
+++ public/app.js
@@ -0,0 +1,30 @@
+/** @type {HTMLInputElement} */
+const gradde = document.getElementById('gradde')
+/** @type {HTMLInputElement} */
+const bla = document.getElementById('bla')
+/** @type {HTMLInputElement} */
+const gron = document.getElementById('gron')
+
+const ratios = {
+    gradde: 1,
+    bla: 1,
+    gron: 0.5,
+}
+
+gradde?.addEventListener('change', e => {
+    const value = e.currentTarget.value
+    bla.value = ratios.bla * value / ratios.gradde
+    gron.value = ratios.gron * value / ratios.gradde
+})
+bla?.addEventListener('change', e => {
+    const value = e.currentTarget.value
+    gradde.value = ratios.gradde * value / ratios.bla
+    gron.value = ratios.gron * value / ratios.bla
+})
+gron?.addEventListener('change', e => {
+    const value = e.currentTarget.value
+    gradde.value = ratios.gradde * value / ratios.gron
+    bla.value = ratios.bla * value / ratios.gron
+})
+
+gradde.dispatchEvent(new Event('change'))
